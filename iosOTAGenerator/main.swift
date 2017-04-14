e68222eb57c85400dc8f852a0d2bafeeea2b4e88
kojiba <kojiba@ro.ru>
2017-04-14 12:40:46 +0300

    Initial Commit

---
+++ iosOTAGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iosOTAGenerator
+//
+//  Created by kojiba on 14.04.17.
+//  Copyright © 2017 kojiba. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
